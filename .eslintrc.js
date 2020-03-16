6a9b6da4588d21672c3bb865490943ba2d91d8be
disco-trooper <1koblizek1@gmail.com>
2020-03-16 20:00:21 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/prettier',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
